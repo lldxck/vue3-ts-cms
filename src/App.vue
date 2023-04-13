e83a89c119b8f3b7c818110a68cfa710e78b36f0
lldxck <895236755@qq.com>
2023-04-13 16:47:55 +0800

    feat: 初始化项目并进行项目规范配置

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>hello vue+typescript</div>
+</template>
+
+<script lang="ts">
+import {
+  defineComponent,
+  ref,
+  computed,
+  reactive,
+  readonly,
+  watch,
+  defineEmits,
+  defineExpose
+} from 'vue'
+
+export default defineComponent({
+  name: 'App'
+})
+</script>
