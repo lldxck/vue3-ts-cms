fe68ecb9bd93aab684f82a0cf78a788d408eaa87
lldxck <895236755@qq.com>
2023-04-14 16:00:13 +0800

    docs: 验证element-plus按需导入

--- components.d.ts
+++ components.d.ts
@@ -10,6 +10,7 @@ export {}
 declare module '@vue/runtime-core' {
   export interface GlobalComponents {
     ElButton: typeof import('element-plus/es')['ElButton']
+    ElRate: typeof import('element-plus/es')['ElRate']
     RouterLink: typeof import('vue-router')['RouterLink']
     RouterView: typeof import('vue-router')['RouterView']
   }
--- src/views/main/main.vue
+++ src/views/main/main.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>main</div>
+  <div>
+    <h4>Color for different levels</h4>
+    <el-rate v-model="value2" :colors="colors" />
+  </div>
 </template>
 
-<script setup lang="ts" name="main"></script>
+<script setup lang="ts" name="main">
+import { ref } from 'vue'
+
+const value2 = ref(null)
+const colors = ref(['#99A9BF', '#F7BA2A', '#FF9900']) // same as { 2: '#99A9BF', 4: { value: '#F7BA2A', excluded: true }, 5: '#FF9900' }
+</script>
 
 <style scoped></style>
