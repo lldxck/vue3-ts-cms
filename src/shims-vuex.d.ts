ed6a4f1c31b883fe8b8f4364f62e28df5bfbe7ec
lldxck <895236755@qq.com>
2023-04-19 10:32:27 +0800

    docs: 修改store的声明文件

--- src/shims-vuex.d.ts
+++ src/shims-vuex.d.ts
@@ -1,10 +1,17 @@
 // vuex-shim.d.ts
+// eslint-disable放在文件第一行，对整个文件生效
 /* eslint-disable */
 import { ComponentCustomProperties } from 'vue'
 import { Store } from 'vuex'
 
 declare module '@vue/runtime-core' {
+  // 声明自己的 store state
+  interface State {
+    [name: string]: any
+  }
+
+  // 为 `this.$store` 提供类型声明
   interface ComponentCustomProperties {
-    $store: Store
+    $store: Store<State>
   }
 }
