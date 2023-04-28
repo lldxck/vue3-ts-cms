ac1bc02a1d51065c60371899ec06796cf8b07778
lldxck <895236755@qq.com>
2023-04-28 14:32:36 +0800

    docs: 删除多余文件以及unocss不生效的另外配置方式

--- src/views/login/cpns/login-account.vue
+++ src/views/login/cpns/login-account.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="login-account">
-    <el-form>
-      <el-form-item>
-        <el-input />
+    <el-form label-width="60px">
+      <el-form-item label="账号">
+        <el-input v-model="account.name" />
       </el-form-item>
-      <el-form-item>
-        <el-input />
+      <el-form-item label="密码">
+        <el-input v-model="account.password" />
       </el-form-item>
     </el-form>
   </div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { reactive } from 'vue'
+const account = reactive({
+  name: '',
+  password: ''
+})
+</script>
 
 <style scoped></style>
--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ const AutoImport = require('unplugin-auto-import/webpack')
 const Components = require('unplugin-vue-components/webpack')
 const { ElementPlusResolver } = require('unplugin-vue-components/resolvers')
 const UnoCSS = require('@unocss/webpack').default
-
+const { presetIcons, presetUno } = require('unocss')
 module.exports = defineConfig({
   // 配置方式一：通过CLI提供给我们的选项来配置
   publicPath: './',
@@ -27,7 +27,15 @@ module.exports = defineConfig({
       Components({
         resolvers: [ElementPlusResolver()]
       }),
-      UnoCSS()
+      UnoCSS({
+        presets: [
+          presetIcons({
+            /* options */
+          }),
+          presetUno() /*使unocss中关于文本的样式生效*/
+          // ...other presets
+        ]
+      })
     ],
     optimization: {
       realContentHash: true
